649a3edd4c4bc999344170e01c067a2518be4c1b
KhashayarB <kbehnam@web.de>
2022-03-13 23:13:34 +0100

    Kash

--- assets/scripts/client.js
+++ assets/scripts/client.js
@@ -1,22 +1,19 @@
 const mysql = require("mysql");
 
-function connection() {
-  this.pool = null;
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "iDoit130322",
+  database: "idoit",
+  port: 3306,
+});
 
-  const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "iDoit130322",
-    database: "idoit",
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "DELETE FROM task WHERE idTask = 1";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 deleted");
   });
-  this.init = function () {
-    this.pool = mysql.createPool(con);
-  };
-  this.acquire = function (callback) {
-    this.init();
-    this.pool.getConnection(function (err, connection) {
-      callback(err, connection);
-    });
-  };
-}
-module.exports = new connection(console.log("connected"));
+});
