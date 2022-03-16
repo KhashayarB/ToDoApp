9a7dd21a2fbf974e55d35646450326f4f199bb6c
KhashayarB <kbehnam@web.de>
2022-03-16 11:16:20 +0100

    Kash

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,15 @@
+submitTask.addEventListener("click", () => {
+  // task.insertAdjacentHTML("afterbegin", newTask);
+  const checkBoxValue = userInput[0].value;
+  const dateTimeValue = userInput[1].value;
+  const descValue = userInput[2].value;
 
-
-addTask.addEventListener("click", () => {
-  console.log("here");
-  task.insertAdjacentHTML("afterbegin", newTask);
+  const newTask = {
+    id: Math.random().toString(),
+    checking: checkBoxValue,
+    date: dateTimeValue,
+    description: descValue,
+  };
+  tasks.push(newTask);
+  console.log(newTask);
 });
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,33 +3,36 @@ const allTask = document.getElementById("allTask");
 const activeTask = document.getElementById("activeTask");
 const cTask = document.getElementById("cTask");
 const submitTask = document.getElementById("submitTask");
-
 const task = document.getElementById("rowTask");
+const userInput = task.querySelectorAll("input");
+const tasks = [];
 
-const newTask =
-  '<div class="row mt-4">' +
-  '<div class="col-1">' +
-  '<div class="col-12 check-box">' +
-  '<form><input type="checkbox" id="i">' +
-  '<label for="i" class="checkbox">' +
-                                '<div class="checkbox__inner">' +
-                                    '<div class="green__ball"></div>' +
-                                '</div>' +
-                            '</label></form>' +
-  " </div>" +
-  " </div>" +
-  '<div class="col-3">' +
-  '<div class="col-12 check-date p-2">' +
-  '<form><input type="datetime-local" required /></form>' +
-  "</div>" +
-  "</div>" +
-  '<div class="col-8">' +
-  '<div class="row check-task p-2">' +
-  '<div class="col-10">' +
-  '<input type="text" placeholder="Your Task Goes Here..." required />' +
-  "</div>" +
-  '<div class="col-2 add-task">' +
-  ' <button id="submitTask"> Submit <i class="fas fa-save"></i> </button>' +
-  "</div>" +
-  "</div>" +
-  "</div>";
+const renderNewTask = (id, checkBox, dateTime, desc) => {
+  task.innerHTML =
+    '<div class="row mt-4">' +
+    '<div class="col-1">' +
+    '<div class="col-12 check-box">' +
+    '<form><input type="checkbox" id="i">' +
+    '<label for="i" class="checkbox">' +
+    '<div class="checkbox__inner">' +
+    '<div class="green__ball"></div>' +
+    "</div>" +
+    "</label></form>" +
+    " </div>" +
+    " </div>" +
+    '<div class="col-3">' +
+    '<div class="col-12 check-date p-2">' +
+    '<form><input type="datetime-local" required /></form>' +
+    "</div>" +
+    "</div>" +
+    '<div class="col-8">' +
+    '<div class="row check-task p-2">' +
+    '<div class="col-10">' +
+    '<input type="text" placeholder="Your Task Goes Here..." required />' +
+    "</div>" +
+    '<div class="col-2 add-task">' +
+    ' <button id="submitTask"> Submit <i class="fas fa-save"></i> </button>' +
+    "</div>" +
+    "</div>" +
+    "</div>";
+};
