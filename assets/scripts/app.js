bd8d707e08198802c443fee73133bd8802bbb9d7
KhashayarB <kbehnam@web.de>
2022-03-16 16:42:35 +0100

    Kash

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,48 @@
 submitTask.addEventListener("click", () => {
   // task.insertAdjacentHTML("afterbegin", newTask);
-  const checkBoxValue = userInput[0].value;
+  let checkBoxValue = userInput[0].value;
   const dateTimeValue = userInput[1].value;
   const descValue = userInput[2].value;
+   toggleCheckBox();
 
-  const newTask = {
-    id: Math.random().toString(),
-    checking: checkBoxValue,
-    date: dateTimeValue,
-    description: descValue,
-  };
-  tasks.push(newTask);
-  console.log(newTask);
+  console.log(cb);
+  //   var btnChecking = document.getElementById("checkbox");
+  //   btnChecking = document.addEventListener("click", function check() {
+  //     btnChecking.checked = true;
+  //   });
+
+  //   btnChecking = document.addEventListener("click", function uncheck() {
+  //     btnChecking.checked = false;
+  //   });
+
+  //   const validateChecking = () => {
+  //     var checkVar = document.querySelectorAll("[name=checkbox]");
+  //     var count = 0;
+  //     const checks = [];
+
+  //     for (var i = 0; i < checkVar.length; i++) {
+  //       if (checkVar[i].checked) {
+  //         count++;
+  //         checks.push(checkVar[i].value);
+  //       }
+  //     }
+  //     console.log(checks);
+  //   };
+
+  //   checkBoxValue = btnChecking;
+  //   function trueCheckbox() {
+  //     checkBoxValue = document.getElementById("checkbox").value = true;
+  //     return checkBoxValue;
+  //   }
+
+     const newTask = {
+       id: Math.random().toString(),
+       checking: checkBoxValue,
+       date: dateTimeValue,
+       description: descValue,
+     };
+     tasks.push(newTask);
+     console.log(tasks);
+    
+   
 });
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,11 +1,20 @@
+//Add button
 const addTask = document.getElementById("addTask");
+// 3 button in header
 const allTask = document.getElementById("allTask");
 const activeTask = document.getElementById("activeTask");
 const cTask = document.getElementById("cTask");
+// submit button
 const submitTask = document.getElementById("submitTask");
+// input
 const task = document.getElementById("rowTask");
 const userInput = task.querySelectorAll("input");
+//array
 const tasks = [];
+//checkBox button
+let cb = document.getElementById("checkbox");
+//delete button
+const deleteTask = document.getElementById("deleteTask");
 
 const renderNewTask = (id, checkBox, dateTime, desc) => {
   task.innerHTML =
@@ -36,3 +45,22 @@ const renderNewTask = (id, checkBox, dateTime, desc) => {
     "</div>" +
     "</div>";
 };
+
+// function toggleCheckBox() {
+//   if (cb.checked === true) {
+//     cb.checked = false;
+//   } else if (cb.checked === false) {
+//     cb.checked = true;
+//   }
+// }
+function toggleCheckBox() {
+  cb.addEventListener("click", () => {
+    if (cb.checked === false) {
+      cb.checked = true;
+    } else if (cb.checked === true) {
+      alert("Your task is completed.");
+    }
+  });
+  submitTask.style.display = "none";
+  deleteTask.style.display = "block";
+}
