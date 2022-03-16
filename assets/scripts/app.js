fc6096f24374576e4e09feb407107ba64ab89891
KhashayarB <kbehnam@web.de>
2022-03-16 18:18:33 +0100

    Kash

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,48 +1,26 @@
 submitTask.addEventListener("click", () => {
-  // task.insertAdjacentHTML("afterbegin", newTask);
-  let checkBoxValue = userInput[0].value;
+  let checkBoxValue = cb.checked;
   const dateTimeValue = userInput[1].value;
   const descValue = userInput[2].value;
-   toggleCheckBox();
+  toggleCheckBox();
 
-  console.log(cb);
-  //   var btnChecking = document.getElementById("checkbox");
-  //   btnChecking = document.addEventListener("click", function check() {
-  //     btnChecking.checked = true;
-  //   });
+  const newTask = {
+    id: Math.random().toString(),
+    checking: checkBoxValue,
+    date: dateTimeValue,
+    description: descValue,
+  };
+  // renderNewTask(
+  //   newTask.id,
+  //   newTask.checking,
+  //   newTask.date,
+  //   newTask.description
+  // );
+  tasks.push(newTask);
+  console.log(tasks);
+});
 
-  //   btnChecking = document.addEventListener("click", function uncheck() {
-  //     btnChecking.checked = false;
-  //   });
-
-  //   const validateChecking = () => {
-  //     var checkVar = document.querySelectorAll("[name=checkbox]");
-  //     var count = 0;
-  //     const checks = [];
-
-  //     for (var i = 0; i < checkVar.length; i++) {
-  //       if (checkVar[i].checked) {
-  //         count++;
-  //         checks.push(checkVar[i].value);
-  //       }
-  //     }
-  //     console.log(checks);
-  //   };
-
-  //   checkBoxValue = btnChecking;
-  //   function trueCheckbox() {
-  //     checkBoxValue = document.getElementById("checkbox").value = true;
-  //     return checkBoxValue;
-  //   }
-
-     const newTask = {
-       id: Math.random().toString(),
-       checking: checkBoxValue,
-       date: dateTimeValue,
-       description: descValue,
-     };
-     tasks.push(newTask);
-     console.log(tasks);
-    
-   
+addTask.addEventListener("click", () => {
+  task.insertAdjacentHTML("afterbegin", renderNewTask());
+  renderNewTask(task);
 });
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -17,33 +17,39 @@ let cb = document.getElementById("checkbox");
 const deleteTask = document.getElementById("deleteTask");
 
 const renderNewTask = (id, checkBox, dateTime, desc) => {
-  task.innerHTML =
-    '<div class="row mt-4">' +
-    '<div class="col-1">' +
-    '<div class="col-12 check-box">' +
-    '<form><input type="checkbox" id="i">' +
-    '<label for="i" class="checkbox">' +
-    '<div class="checkbox__inner">' +
-    '<div class="green__ball"></div>' +
-    "</div>" +
-    "</label></form>" +
-    " </div>" +
-    " </div>" +
-    '<div class="col-3">' +
-    '<div class="col-12 check-date p-2">' +
-    '<form><input type="datetime-local" required /></form>' +
-    "</div>" +
-    "</div>" +
-    '<div class="col-8">' +
-    '<div class="row check-task p-2">' +
-    '<div class="col-10">' +
-    '<input type="text" placeholder="Your Task Goes Here..." required />' +
-    "</div>" +
-    '<div class="col-2 add-task">' +
-    ' <button id="submitTask"> Submit <i class="fas fa-save"></i> </button>' +
-    "</div>" +
-    "</div>" +
-    "</div>";
+  task.innerHTML = `
+  <div class="row mt-4">
+                <div class="col-1">
+                    <div class="col-12 check-box">
+                        <form><input  type="checkbox" name="checkbox" value="${checkBox}"
+                                id="checkbox">
+                                <input type="hidden" value="${id}">
+                            <label for="checkbox" class="checkbox">
+                                <div class="checkbox__inner">
+                                    <div class="green__ball"></div>
+                                </div>
+                            </label>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="col-12 check-date p-2">
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="row check-task p-2">
+                        <div class="col-10">
+                            <input type="text" placeholder="Your Task Goes Here..." value="${desc}" />
+                        </div>
+                        <div class="col-2 d-flex justify-content-end add-task">
+                            <button id="submitTask"> Submit <i class="fas fa-save"></i>
+                            </button>
+                            <button id="deleteTask"> Delete <i class="fas fa-trash"></i> </button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>`;
 };
 
 // function toggleCheckBox() {
