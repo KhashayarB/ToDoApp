225baa3cddf96d6733105a4a3b3829ea7d6b2751
sohrab <sohrabnasehi.sn@gmail.com>
2022-03-18 15:41:31 +0330

    final

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,35 @@
 submitTask.addEventListener("click", () => {
-  // task.insertAdjacentHTML("afterbegin", newTask);
+  // task.insertAdjacentHTML("afterbegin",renderNewTask());
   const checkBoxValue = userInput[0].value;
   const dateTimeValue = userInput[1].value;
   const descValue = userInput[2].value;
 
+  toggleCheckBox();
+
   const newTask = {
-    id: Math.random().toString(),
+    id: Math.floor(Math.random() * 9999).toString(),
     checking: checkBoxValue,
     date: dateTimeValue,
     description: descValue,
   };
   tasks.push(newTask);
   console.log(newTask);
+});
+
+function toggleCheckBox() {
+  cb.addEventListener("click", () => {
+    if (cb.checked === false) {
+      cb.checked = true;
+    }
+    if (cb.checked === true) {
+      alert("Your task is completed.")
+    }
+  });
+  submitTask.style.display = "none";
+  deleteTask.style.display = "block";
+};
+
+
+addTask.addEventListener('click', () => {
+  renderNewTask();
 });
